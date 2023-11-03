fe94bb2626f2b4f91fc7a075c7dfd4c2bae00a11
Johan <johan@ftm.nl>
2023-11-03 21:53:40 +0100

    clean up token logic

--- cli/insight/__init__.py
+++ cli/insight/__init__.py
@@ -13,10 +13,60 @@ class AuthException(Exception):
     pass
 
 
+def set_tokens(data):
+    for token in ("refresh_token", "access_token"):
+        keyring.set_password("insight", token, data[token])
+
+
+def authorize_device():
+    session = Session()
+    res = session.post(
+        config["auth"]["device-endpoint"],
+        data={"client_id": config["auth"]["client-id"]},
+    )
+    body = res.json()
+    webbrowser.open(body["verification_uri_complete"])
+
+    until_time = time.time() + body["expires_in"]
+    while until_time > time.time():
+        res = session.post(
+            config["auth"]["token-endpoint"],
+            data={
+                "client_id": config["auth"]["client-id"],
+                "device_code": body["device_code"],
+                "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+            },
+        )
+        if res.status_code == 200:
+            break
+        time.sleep(body["interval"])
+
+    set_tokens(res.json())
+
+
+def refresh_token():
+    session = Session()
+    res = session.post(
+        config["auth"]["token-endpoint"],
+        data={
+            "client_id": config["auth"]["client-id"],
+            "refresh_token": keyring.get_password("insight", "refresh_token"),
+            "grant_type": "refresh_token",
+        },
+    )
+    if res.status_code == 400 and res.json()["error"] == "invalid_grant":
+        keyring.delete_password("insight", "refresh_token")
+    else:
+        set_tokens(res.json())
+
+
 class OAuthSession(Session):
     def request(self, *args, **kwargs):
-        token = load_token()
-        self.headers["Authorization"] = f"Bearer {token['access_token']}"
+        token = keyring.get_password("insight", "access_token")
+        if token is None:
+            raise AuthException('Unauthenticated, run "insight login"')
+
+        self.headers["Authorization"] = f"Bearer {token}"
 
         res = super().request(*args, **kwargs)
         if res.status_code == 401 and res.json()["message"] == "JWT expired":
@@ -40,30 +90,6 @@ else:
     config.read(config_file)
 
 
-def refresh_token():
-    token = load_token()
-    session = Session()
-    data = {
-        "client_id": config["auth"]["client-id"],
-        "refresh_token": token["refresh_token"],
-        "grant_type": "refresh_token",
-    }
-    res = session.post(config["auth"]["token-endpoint"], data=data)
-    if res.status_code == 400 and res.json()["error"] == "invalid_grant":
-        delete_token()
-
-
-def delete_token():
-    keyring.delete_password("insight", "insight")
-
-
-def load_token():
-    token = keyring.get_password("insight", "insight")
-    if token is None:
-        raise AuthException('Not authenticated, see "insight login"')
-    return json.loads(token)
-
-
 @click.group()
 def cli():
     pass
@@ -71,28 +97,13 @@ def cli():
 
 @cli.command()
 def login():
-    data = {"client_id": config["auth"]["client-id"]}
-
-    session = Session()
-    res = session.post(config["auth"]["device-endpoint"], data=data)
-    body = res.json()
-    webbrowser.open(body["verification_uri_complete"])
-    data["device_code"] = body["device_code"]
-    data["grant_type"] = "urn:ietf:params:oauth:grant-type:device_code"
-
-    until_time = time.time() + body["expires_in"]
-    while until_time > time.time():
-        res = session.post(config["auth"]["token-endpoint"], data=data)
-        if res.status_code == 200:
-            break
-        time.sleep(body["interval"])
-
-    keyring.set_password("insight", "insight", res.text)
+    authorize_device()
 
 
 @cli.command()
 def logout():
-    delete_token()
+    for token in ("refresh_token", "access_token"):
+        keyring.delete_password("insight", token)
 
 
 @cli.command()
